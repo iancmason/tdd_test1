100147e77c9596ea8388ebc46679b4ef4d07150b
Ian <ian.christopher.mason@gmail.com>
2020-01-15 21:00:03 -0600

    refactoring

--- src/stock_alerter/tests/test_stock.py
+++ src/stock_alerter/tests/test_stock.py
@@ -21,3 +21,14 @@ class StockTest(unittest.TestCase):
         self.goog.update(datetime(2020, 2, 12), price=10)
         self.goog.update(datetime(2020, 2, 13), price=8.4)
         self.assertAlmostEqual(8.4, self.goog.price, delta=0.0001)
+
+class StockTrendTest(unittest.TestCase):
+    def setUp(self):
+        self.goog = Stock("GOOG")
+
+    def test_increasing_trend_is_true_if_price_increases_for_3_updates(self):
+        timestamps = [datetime(2020, 2, 11), datetime(2020, 2, 12), datetime(2020, 2, 13)]
+        prices = [8, 10, 12]
+        for timestamp, price in zip(timestamps, prices):
+            self.goog.update(timestamp, price)
+        self.assertTrue(self.goog.is_increasing_trend())
