f985a5552afa7d2e1afca411d2b708307f9f09f9
Ian <ian.christopher.mason@gmail.com>
2020-01-21 21:43:07 -0600

    adding rule class and tests

---
+++ src/stock_alerter/rule.py
@@ -0,0 +1,14 @@
+class PriceRule:
+  def __init__(self, symbol, condition):
+    self.symbol = symbol
+    self.condition = condition
+
+  def matches(self, exchange):
+    try:
+      stock = exchange[self.symbol]
+    except KeyError:
+      return False
+    return self.condition(stock) if stock.price else False
+
+  def depends_on(self):
+    return {self.symbol}
